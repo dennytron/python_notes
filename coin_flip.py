3a6498e1a4ef304577affd890993c53b60dd8072
Dennis Follensbee <da.follensbee@gmail.com>
2020-06-16 09:53:55 -0400

    Create coin_flip.py

---
+++ coin_flip.py
@@ -0,0 +1,30 @@
+import time
+from concurrent import futures
+from os import cpu_count, getpid
+from random import getrandbits, choice
+
+
+def rando(x):
+    print(f"work = {x}, pid = {getpid()}")
+    return sum(getrandbits(1) for _ in range(x))
+
+
+def main():
+    chunks = cpu_count()
+    total_work = 1000000000
+    work = [int(total_work / chunks)] * chunks
+
+    with futures.ProcessPoolExecutor() as executor:
+        totes = sum(executor.map(rando, work))
+
+    print(totes / total_work)
+
+
+if __name__ == "__main__":
+    print(f"cpu_count = {cpu_count()}")
+
+    t = time.perf_counter()
+    main()
+    a = time.perf_counter()
+
+    print(a - t)
